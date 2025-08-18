e21565b5b85b9dbe3086112e654d0a6f3e724cf3
Victor Orozco <me@vorozco.com>
2025-08-18 11:29:54 -0300

    Fixing dead code bug

--- quarkus-cloud-native-workload/src/main/java/com/vorozco/GreetingResource.java
+++ quarkus-cloud-native-workload/src/main/java/com/vorozco/GreetingResource.java
@@ -11,14 +11,12 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        String result = doSpotBugsDemo(); // Same error
         return "Hello from Quarkus REST";
 
     }
 
     private String doSpotBugsDemo(){
         // SpotBugs: DLS_DEAD_LOCAL_STORE - The value assigned to 'result' is never read
-        String result = "This value is never used";
         return "SpotBugs demo";
     }
 }
