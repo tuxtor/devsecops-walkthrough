bb74caea9ce31e5a72da50718a1c201dabbc72fd
Victor Orozco <me@vorozco.com>
2025-08-18 18:37:15 -0300

    Add explicit dep between helm chart for kube-prometheus

--- aws-infra/helm/kube-prometheus.tf
+++ aws-infra/helm/kube-prometheus.tf
@@ -5,5 +5,7 @@ resource "helm_release" "kube-prometheus-stack" {
   create_namespace  = true
   namespace         = "kube-prometheus-stack"
   wait_for_jobs     = true
+
+  depends_on = [helm_release.aws-load-balancer-controller]
 }
 
