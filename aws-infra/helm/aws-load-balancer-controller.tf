e811c027a7d19007943c3a34f64d47d7ffc9fd61
Victor Orozco <me@vorozco.com>
2025-08-17 22:59:13 -0300

    Fixing full destroy

--- aws-infra/helm/aws-load-balancer-controller.tf
+++ aws-infra/helm/aws-load-balancer-controller.tf
@@ -9,11 +9,12 @@ resource "kubernetes_service_account" "aws-load-balancer-controller" {
 }
 
 resource "helm_release" "aws-load-balancer-controller" {
-  name       = "aws-load-balancer-controller"
-  repository = "https://aws.github.io/eks-charts"
-  chart      = "aws-load-balancer-controller"
-  namespace  = "kube-system"
-  timeout    = 900
+  name          = "aws-load-balancer-controller"
+  repository    = "https://aws.github.io/eks-charts"
+  chart         = "aws-load-balancer-controller"
+  namespace     = "kube-system"
+  timeout       = 900
+  wait_for_jobs = true
   set {
     name  = "clusterName"
     value = var.cluster_name
--- aws-infra/helm/ingress-nginx.tf
+++ aws-infra/helm/ingress-nginx.tf
@@ -1,21 +1,15 @@
-resource "kubernetes_namespace" "ingress-nginx" {
-  metadata {
-    name = "ingress-nginx"
-  }
-}
-
 resource "helm_release" "ingress-nginx" {
   name             = "ingress-nginx"
   repository       = "https://kubernetes.github.io/ingress-nginx"
   chart            = "ingress-nginx"
-  create_namespace = false
+  create_namespace = true
   namespace        = "ingress-nginx"
   timeout          = 900
   values = [
     file("${path.module}/ingress-nginx-values.yaml")
   ]
   wait_for_jobs = true
-  depends_on = [kubernetes_namespace.ingress-nginx, helm_release.aws-load-balancer-controller]
+  depends_on = [helm_release.aws-load-balancer-controller]
 }
 
 resource "kubernetes_manifest" "ingress-nginx-alb" {
@@ -24,8 +18,13 @@ resource "kubernetes_manifest" "ingress-nginx-alb" {
       file("${path.module}/ingress-nginx-alb.yml"), "alb_certificate_arns", var.acm_certificate_arn
     )
   )
-  depends_on = [helm_release.ingress-nginx, helm_release.aws-load-balancer-controller]
-  count = var.infra_bootstrap ? 0 : 1
+
+  wait {
+    fields = {
+      "status.loadBalancer.ingress[0].hostname" = null
+    }
+  }
+  depends_on = [helm_release.ingress-nginx]
 }
 
 data "aws_lb" "ingress-nginx" {
