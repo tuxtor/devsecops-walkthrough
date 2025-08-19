e60291c5648080a708d12aa530533ae2fb3eed12
Victor Orozco <me@vorozco.com>
2025-08-19 20:59:16 -0300

    Esta es la demo en vivo

--- quarkus-cloud-native-workload/src/main/java/com/vorozco/GreetingResource.java
+++ quarkus-cloud-native-workload/src/main/java/com/vorozco/GreetingResource.java
@@ -11,12 +11,14 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        String saludo = doSpotBugsDemo();
         return "Hello from Quarkus REST";
 
     }
 
     private String doSpotBugsDemo(){
         // SpotBugs: DLS_DEAD_LOCAL_STORE - The value assigned to 'result' is never read
+        String hola = "Hola Mundo";
         return "SpotBugs demo";
     }
 }
