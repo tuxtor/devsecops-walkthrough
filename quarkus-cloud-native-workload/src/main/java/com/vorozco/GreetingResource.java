6bd19fe4c0ef86f55bdb5358f3dd7ffe14dc12ff
Victor Orozco <me@vorozco.com>
2025-09-29 18:05:55 -0300

    Cambiando saludo

--- quarkus-cloud-native-workload/src/main/java/com/vorozco/GreetingResource.java
+++ quarkus-cloud-native-workload/src/main/java/com/vorozco/GreetingResource.java
@@ -11,7 +11,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        return "Hola amigos del ingenio";
     }
 
     private String doSpotBugsDemo(){
