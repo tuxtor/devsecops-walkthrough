8189befce100c3b735649f93c24af93f3274aa74
Victor Orozco <me@vorozco.com>
2025-09-29 18:12:05 -0300

    Arreglando el codigo fuente para satisfacer la prueba en Java

--- quarkus-cloud-native-workload/src/main/java/com/vorozco/GreetingResource.java
+++ quarkus-cloud-native-workload/src/main/java/com/vorozco/GreetingResource.java
@@ -11,7 +11,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hola amigos del ingenio";
+        return "Hello from Quarkus REST";
     }
 
     private String doSpotBugsDemo(){
